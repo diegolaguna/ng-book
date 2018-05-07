912f2b1e44b8b9e2832cdff47e653675ab51c285
Diego Laguna <diego.laguna@gmail.com>
2018-05-06 23:50:46 -0300

    Inventory App

---
+++ inventory-app/src/app/products-list/products-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'products-list',
+  templateUrl: './products-list.component.html'
+})
+
+export class ProductsListComponent {
+
+  @Input() productList: Product[];
+  @Output() onProductSelected: EventEmitter<Product>
+    
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
